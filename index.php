09c8820ed1ca8464ab43b9141c8ccbd057158fe7
Ian Mckay <ian.mckay@foundrycomms.co.uk>
2016-07-27 12:42:27 +0100

    Initialised repo

---
+++ index.php
@@ -0,0 +1,170 @@
+<?phpSpaceZero
+?>
+
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html class="ie ie6 nojs Corporate
+">
+<![endif]-->
+<!--[if IE 7]>
+<html class="ie ie7 nojs Corporate
+">
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8 nojs Corporate
+">
+<![endif]-->
+<!--[if !(IE)]><!-->
+<html class="nojs" lang="en">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="initial-scale=1.0,width=device-width">
+<meta name="msvalidate.01" content="12CA65DABA62FBA74F1C4C0248D90FC2">
+<title>The Foundry - Design, advertising and digital agency in Manchester</title>
+<meta name="description" content="Footfall description goes here" />
+<link rel="canonical" href="/" />
+<meta name="keywords" content="Keywords" />
+
+
+<link rel="stylesheet" media="all" href="/css/style.css">
+<link rel="stylesheet" media="all" href="/css/animate.css">
+
+
+<script type="text/javascript" src="/js/libraries/jquery.js"></script>
+<script type="text/javascript" src="/js/libraries/yepnope.js"></script>
+<script type="text/javascript" src="/js/common.js"></script>
+
+<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="/js/libraries/waypoints/lib/jquery.waypoints.min.js"></script>
+
+<script type="text/javascript" src="http://fast.fonts.net/jsapi/9f5f52e2-17aa-426c-a474-9f4fe51d8b1f.js"></script>
+
+</head>
+<body>
+
+<main role="main">
+	<header role="banner">
+		<div class="l-content">
+			<div class="single">
+				<div class="logo">
+					<a href="/"><img src="/img/foundry-logo.png" alt="The Foundry Logo"></a>
+				</div> <!-- logo -->
+				<div class="navigation" id="navigation">
+				 	<nav role="navigation" aria-label="Main Navigation">
+				 	<ul class="navigation-top">
+						<li><a href="#">Work</a></li>
+						<li><a href="#">Service</a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">News</a></li>
+						<li><a href="#">Careers</a></li>
+						<li><a href="#">Contact</a></li>
+				 	</ul>
+				 	</nav>
+				 </div> <!-- navigation -->
+			 </div> <!-- single -->
+		 </div> <!-- l-content -->
+	 </header>
+	
+	
+	
+	
+	<div class="l-masthead mastheadfull animated fadeIn">
+		<ul class="rslides">
+			<li>
+				<a href="#">
+					<div class="mastheadfull-info">
+						<div class="l-content mastheadfull-info-container">
+							<div class="single">
+								<div class="mastheadfull-info-content">
+									<div class="rule">
+										<img src="/img/arrows-buttons/rule.png" alt="Rule" /></video>
+									</div>
+									<h1>New agency showreel</h1>
+									<div class="button-next">
+										<p>Play</p>
+										<img src="/img/arrows-buttons/arrow-right.png" alt="Show me the showreel" />
+									</div>
+								</div> <!-- mastheadfull-info-content -->
+							</div> <!-- single -->
+						</div>  <!-- l-content -->
+					</div> <!-- mastheadfull-info -->
+					<div class="mastheadfull-visual">
+						<ul class="rslides">
+							<li><video class="video" autoplay muted loop><source src="/img/video.mp4" type="video/mp4"></video></li>
+						</ul>
+					</div> <!-- mastheadfull-visual -->
+				</a>
+			</li>
+		</ul>
+	</div> <!-- mastheadfull -->
+	
+	<div class="l-content case-studies">
+		<div class="groupthree">
+			<div class="groupthree-content">
+				<img src="/img/home-page/case-studies/ppcp.jpg" alt="PPCP Case Study" class="animated fadeIn" />
+			</div>
+			<div class="groupthree-content">
+				<img src="/img/home-page/case-studies/ppcp.jpg" alt="PPCP Case Study" />
+			</div>
+			<div class="groupthree-content">
+				<img src="/img/home-page/case-studies/ppcp.jpg" alt="PPCP Case Study" />
+			</div>
+			<div class="groupthree-content">
+				<img src="/img/home-page/case-studies/ppcp.jpg" alt="PPCP Case Study" />
+			</div>
+			<div class="groupthree-content">
+				<img src="/img/home-page/case-studies/ppcp.jpg" alt="PPCP Case Study" />
+			</div>
+			<div class="groupthree-content">
+				<img src="/img/home-page/case-studies/ppcp.jpg" alt="PPCP Case Study" />
+			</div>
+		</div>
+	</div>
+	
+	<footer role="contentinfo">
+		<div class="l-content">
+			<div class="single">
+				<div class="toggle">
+					<div class="telephone">
+						<p><a href="tel:+443333234570">0333 323 4570</a></p>
+					</div> <!-- social -->
+					<div class="navigation" id="navigation">
+						<nav role="navigation" aria-label="Main Navigation">
+							<ul class="navigation-top">
+								<li><a href="/privacy/">Privacy Policy</a></li>
+							</ul>
+						</nav>
+					</div> <!-- navigation -->
+					<div class="social">
+					<!-- <ul class="navigation-social">
+							<li class="last"><a href="#"><img src="/img/header/social/icon-facebook.png" alt="Footfall Facebook"></a></li>
+							<li><a href="#"><img src="/img/header/social/icon-twitter.png" alt="Footfall Twitter"></a></li>
+							<li><a href="#"><img src="/img/header/social/icon-youtube.png" alt="Footfall YouTube"></a></li>
+						</ul> -->
+					</div> <!-- social -->
+				</div> <!-- toggle -->
+			</div> <!-- single -->
+		</div> <!-- l-content -->
+	</footer>
+</main>
+
+
+<script src="/js/libraries/jquery.scrolldepth.min.js"></script>
+<script src="/js/libraries/jquery.magnific-popup.js"></script>
+<script src="/js/libraries/jquery.tipsy.js"></script>
+<script src="/js/libraries/responsiveslides.js"></script>
+<script src="/js/libraries/Validation/jquery.validate.min.js"></script>
+<script src="/js/libraries/jquery.ui.js"></script>
+<script src="js/enquire.js"></script>
+<script src="js/owl.carousel.js"></script>
+<script src="js/jquery.imagesloaded.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/tooltip.js"></script>
+<script src="js/transit.js"></script>
+<script src="js/transition.js"></script>
+<script src="js/scrollspy.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+</html>
